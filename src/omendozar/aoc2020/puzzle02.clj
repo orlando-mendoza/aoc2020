6c265288074c2a3be2d383848de50c4291dd633c
Orlando Mendoza <orlandomr27@gmail.com>
2020-12-04 12:10:56 -0600

    day2 done!

--- src/omendozar/aoc2020/puzzle02.clj
+++ src/omendozar/aoc2020/puzzle02.clj
@@ -7,10 +7,13 @@
 1-3 b: cdefg
 2-9 c: ccccccccc")
 
+;; ------------------------------------------------------------
+;; Helper Funcions
+
 (defn- parse-line
   [s]
   (let [[_ min max letter pass] (re-find #"(\d+)-(\d+) (.): (.*)" s)]
-    [(Long/parseLong min) (Long/parseLong max) letter pass]))
+    [(Long/parseLong min) (Long/parseLong max) (first letter) pass]))
 
 (defn serialize-input
   [input]
@@ -18,83 +21,45 @@
        str/split-lines
        (map #(parse-line %))))
 
-(defn valid-password-part-1?
-  [s]
-  (let [_ (println s)
-        freq (frequencies (get s 3))
-        letter (char (first (.getBytes (get s 2))))
-        min (get s 0)
-        max (get s 1)
-        times (get freq letter 0)]
-    (if (and (>= times min) (<= times max))
-      true
-      false)))
-
-(defn valid-password-part-2
-  [s]
-  (let [_ (println s)
-        letter (char (first (.getBytes (get s 2))))
-        pos1 (get s 0)
-        pos2 (get s 1)
-        ;; TODO: Check if IndexOf letter is equal to pos1 or pos2 (not both)
-        ]))
-
-(defn get-valid-passwords
-  [vector-of-vectors]
-  (for [v vector-of-vectors
-        :when (valid-password-part-1? v)]
-    (into [] v)))
-
-(defn main
-  [input]
-  (->> input
-       serialize-input
-       get-valid-passwords
-       count))
+;; ------------------------------------------------------------
+;; Get input from file
 
 (def input
   (str (slurp (clojure.java.io/reader "resources/puzzle02_input.txt"))))
 
-(main input)
-;; => 434;
+;; ------------------------------------------------------------
+;; PART ONE
+
+(->> input
+     serialize-input
+     (filter (fn [[min max letter password]]
+               (<= min (get (frequencies password) letter 0) max)))
+     count)
+;; => 434
 
 ;; ------------------------------------------------------------
 ;; PART TWO
 
-
+(->> input
+     serialize-input
+     (filter (fn [[pos1 pos2 letter password]]
+               (not= (= (nth password (dec pos1)) letter)
+                     (= (nth password (dec pos2)) letter))))
+     count)
+;; => 509
 
 (comment
-  (def s [1 3 "a" "abcde"])
-  (valid-password? s )
-  (valid-password? [1 3 "a" "abcdesaaa"] )
-  (def s (serialize-input sample-input))
-  (count (list-of-passwords s))
-
-  (def ss (serialize-input input))
-  (count (list-of-passwords ss))
-
-  (for [v '([1 3 "a" "abcde"] [1 3 "b" "cdefg"] [2 9 "c" "ccccccccc"])
-        :when (valid-password? v)]
-    (into [] v))
-  ;; => (2) ;; => ([2 9 "c" "ccccccccc"])
-
-  (for [x '([:a 1] [:b 2] [:c 0]) :when (= (get x 1) 0) ]  x)
-  ;; => ([:c 0])
-
-  (def pm (map valid-password? '([1 3 "a" "abcde"] [1 3 "b" "cdefg"] [2 9 "c" "ccccccccc"])))
-  (filter even? [1 2 3 4])
-
-  (first '([1 3 "a" "abcde"] [1 3 "b" "cdefg"] [2 9 "c" "ccccccccc"]))
-  ;; => [1 3 "a" "abcde"]
-
-  (first '([1 3 "a" "abcde"]))
-  ;; => [1 3 "a" "abcde"]
-
-  (parse-line s)  ;; => [1 3 "a" "abcde"]
+  ;; Part two
+  (nth "achasdfk" (dec 1))
+  ;; ------------------------------------------------------------
 
   (char (first (.getBytes "a")))
+  ;; => \a
+  (first "a")
+  (apply char "a")
 
   (let [bytes-array (.getBytes "abc")]
     (char (first bytes-array)))
 
   (re-find #"(\d+)-(\d+) (.): (.*)" "1-3 a: abcde"))
+;; => nil;; => nil
