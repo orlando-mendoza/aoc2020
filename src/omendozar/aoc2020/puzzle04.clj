180c9dd0505f4553a0d52dc0f54381f21a2f1b06
Orlando Jose Mendoza Rivera <omendozar@Orlandos-MacBook-Pro-2.local>
2020-12-09 08:10:13 -0600

    WIP: puzzle04 parsing input

--- src/omendozar/aoc2020/puzzle04.clj
+++ src/omendozar/aoc2020/puzzle04.clj
@@ -1,6 +1,7 @@
 (ns omendozar.aoc2020.puzzle04
   (:require
-    [clojure.string :as str]))
+    [clojure.string :as str]
+    [clojure.java.io :as io]))
 
 (def demo-input "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
 byr:1937 iyr:2017 cid:147 hgt:183cm
@@ -16,26 +17,47 @@ hgt:179cm
 hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in")
 
+(def input (slurp (io/resource "puzzle04_input.txt")))
+
+(defn parse-entry [entry]
+  (into {} (map #(str/split % #":") (str/split entry #"\s"))))
+
+(defn parse-input [input]
+  (->> (str/split input "\n\n")
+       (map parse-entry)))
+
+(defn parse-line [line]
+  (let [coll (vec (re-seq #"(\S+):(\S+)"
+                          line))
+        _ (clojure.pprint/pprint coll)]
+    (map #(subvec % 1 3) coll)) )
+
 (->> demo-input
      (#(str/split % #"\n\n"))
-     (#(str/split % #"\s")))
+     (#(map parse-line %)))
 
-;; (mapv #(str/replace % #"\n" \space))
-;; (re)
+(map #(map parse-line %) (str/split demo-input #"\n\n"))
 
-(->> (str/split demo-input #"\n\n"))
+
+(map parse-line)
 
 (comment
+  (parse-input input)
+  (parse-input demo-input)
+
   (def data ["ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm"
              "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929"
              "hcl:#ae17e1 iyr:2013\neyr:2024\necl:brn pid:760753108 byr:1931\nhgt:179cm"
              "hcl:#cfa07d eyr:2025 pid:166559648\niyr:2011 ecl:brn hgt:59in"])
-  (apply map (#(str/replace % #"\n" \space) %) data)
 
-  (map #(str/split \space %) data)
+  ;; this one is working
 
-  (def result
-    (let [[_ k v] (re-seq #"(\S+):(\S+)"
-                          "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm")]
-      into {} {k v}))
+  (parse-line "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm")
+
+  (map #(subvec % 1 3) result)
+
+  (rest ["ecl:gry" "ecl" "gry"])
+  (subvec ["ecl:gry" "ecl" "gry"] 1 3)
+
   )
+
