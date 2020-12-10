807685f27406f5542c983e365d373ec2bb7bdfa0
Orlando Mendoza <orlandomr27@gmail.com>
2020-12-10 10:22:38 -0600

    puzzle 04 part 1 finished!

--- src/omendozar/aoc2020/puzzle04.clj
+++ src/omendozar/aoc2020/puzzle04.clj
@@ -19,45 +19,36 @@ iyr:2011 ecl:brn hgt:59in")
 
 (def input (slurp (io/resource "puzzle04_input.txt")))
 
-(defn parse-entry [entry]
-  (into {} (map #(str/split % #":") (str/split entry #"\s"))))
+(defn parse-entry
+  [entry]
+  (->> (re-seq #"(\S+):(\S+)" entry)
+       (into {} (map #(subvec % 1 3)))))
 
-(defn parse-input [input]
-  (->> (str/split input "\n\n")
-       (map parse-entry)))
+(defn passport?
+  [m]
+  (= (count (dissoc m "cid")) 7))
 
-(defn parse-line [line]
-  (let [coll (vec (re-seq #"(\S+):(\S+)"
-                          line))
-        _ (clojure.pprint/pprint coll)]
-    (map #(subvec % 1 3) coll)) )
+;; ------------------------------------------------------------
+;; Count demo-input
 
-(->> demo-input
-     (#(str/split % #"\n\n"))
-     (#(map parse-line %)))
+(->> (str/split demo-input #"\n\n")
+     (map parse-entry)
+     (filter passport?)
+     count)
+;; => 2
 
-(map #(map parse-line %) (str/split demo-input #"\n\n"))
+;; ------------------------------------------------------------
+;; Count real input
+
+(->> (str/split input #"\n\n")
+     (map parse-entry)
+     (filter passport?)
+     count)
+;; => 204
 
 
-(map parse-line)
+
+
 
 (comment
-  (parse-input input)
-  (parse-input demo-input)
-
-  (def data ["ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm"
-             "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929"
-             "hcl:#ae17e1 iyr:2013\neyr:2024\necl:brn pid:760753108 byr:1931\nhgt:179cm"
-             "hcl:#cfa07d eyr:2025 pid:166559648\niyr:2011 ecl:brn hgt:59in"])
-
-  ;; this one is working
-
-  (parse-line "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm")
-
-  (map #(subvec % 1 3) result)
-
-  (rest ["ecl:gry" "ecl" "gry"])
-  (subvec ["ecl:gry" "ecl" "gry"] 1 3)
-
-  )
-
+)
