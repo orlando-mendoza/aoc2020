fc6a78cb647fcae9f374a4e061fff874025a041d
Orlando Jose Mendoza Rivera <omendozar@Orlandos-MacBook-Pro-2.local>
2020-12-10 12:16:45 -0600

    puzzle04 part 2 finished!

--- src/omendozar/aoc2020/puzzle04.clj
+++ src/omendozar/aoc2020/puzzle04.clj
@@ -46,8 +46,42 @@ iyr:2011 ecl:brn hgt:59in")
      count)
 ;; => 204
 
+;; ------------------------------------------------------------
+;; PART TWO
 
+;; byr (Birth Year) - four digits; at least 1920 and at most 2002.
+;; iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+;; eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+;; hgt (Height) - a number followed by either cm or in:
+;; If cm, the number must be at least 150 and at most 193.
+;; If in, the number must be at least 59 and at most 76.
+;; hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+;; ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+;; pid (Passport ID) - a nine-digit number, including leading zeroes.
+;; cid (Country ID) - ignored, missing or not.
 
+(defn parse-long [l]
+     (Long/parseLong l))
+
+(defn valid-passport?
+  [{:strs [byr iyr eyr hgt hcl ecl pid cid]}]
+  (and
+   byr (<= 1920 (parse-long byr) 2002)
+   iyr (<= 2010 (parse-long iyr) 2020)
+   eyr (<= 2020 (parse-long eyr) 2030)
+   hgt (let [[_ num unit] (re-find #"(\d+)(in|cm)" hgt)]
+         (case unit
+           "cm" (<= 150 (parse-long num) 193)
+           "in" (<= 59 (parse-long num) 76)
+           false))
+   hcl (re-find #"^#[0-9a-f]{6}$" hcl)
+   ecl (#{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"} ecl)
+   pid (re-find #"^\d{9}$" pid)))
+
+(->> (str/split input #"\R\R")
+     (map parse-entry)
+     (filter valid-passport?)
+     count)
 
 
 (comment
